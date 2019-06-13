3e1d27c112f171efe7770fa86e08a46aeb10f31f
yanis.telaoumaten <yanis.telaoumaten@sterblue.com>
2019-06-13 21:09:09 +0200

    Use webpack for lighter weight

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const path = require("path");
+
+const config = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "lib/"),
+    filename: "genalgo.js",
+    library: "genalgo",
+    libraryTarget: "umd"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-flow"],
+            plugins: ["@babel/plugin-transform-runtime"]
+          }
+        }
+      }
+    ]
+  },
+  optimization: {
+    minimizer: [new UglifyJsPlugin()]
+  }
+};
+module.exports = config;
